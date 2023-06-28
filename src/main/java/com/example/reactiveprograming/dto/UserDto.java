12fea73f9ba7720786cd61c28f3e161d205f708c
Subodh Bhandari <subodh.bhandari@sbsolutionsnepal.com>
2023-06-28 11:04:03 +0545

    added userDto

---
+++ src/main/java/com/example/reactiveprograming/dto/UserDto.java
@@ -0,0 +1,18 @@
+package com.example.reactiveprograming.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Subodh Bhandari on 6/28/2023
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDto {
+
+    private String firstName;
+    private String lastName;
+}
